328c97bd54716f5230ad16446694aeb2fb3b099c
yesol-choi <ysoli3651@naver.com>
2021-11-07 16:00:49 +0900

    mongoose

---
+++ blog-backend/src/api/posts/posts.ctrl.js
@@ -0,0 +1,86 @@
+import Post from '../../models/post';
+/*
+    POST /api/posts
+    {
+        title:'제목',
+        body:'내용',
+        tags: ['태그1', '태그2']
+    }
+*/
+export const write = async ctx => {
+    const {title, body, tags} = ctx.request.body;
+    const post = new Post({
+        title,
+        body,
+        tags,
+    });
+    try{
+        await post.save();
+        ctx.body = post;
+    }catch(e){
+        ctx.throw(500, e);
+    };
+};
+/*
+    GET /api/posts
+*/
+export const list = async ctx => {
+    try{
+        const posts = await Post.find().exec();
+        ctx.body = posts;
+    }catch(e) {
+        ctx.throw(500, e);
+    }
+};
+/*
+    GET /api/posts/:id
+*/
+export const read = async ctx => {
+    const {id} = ctx.params;
+    try{
+        const {id} = ctx.params;
+        if(!post) {
+            ctx.status = 404; //Not Found
+            return;
+        }
+        ctx.body = post;
+    }catch(e){
+        ctx.throw(500, e);
+    }
+};
+/*
+    DELETE /api/posts/:id
+*/
+export const remove = async ctx => {
+    const {id} = ctx.params;
+    try{
+        await Post.findByIdAndRemove(id).exec();
+        ctx.status = 204; //No Content (성공하기는 했지만 응답할 데이터는 없음)
+    } catch(e) {
+        ctx.throw(500,e);
+    }
+};
+/*
+    PATCH /api/posts/:id
+    {
+        title:'수정',
+        body: '수정 내용',
+        tags: ['수정', '태그']
+    }
+*/
+export const update = async ctx => {
+    const {id} = ctx.params;
+    try{
+        const post = await Post.findByIdAndUpdate(id, ctx.request.body, {
+            new: true, //이 값을 설정하면 업데이트된 데이터를 반환합니다.
+            //false일 때는 업데이트되기 전의 데이터를 반환합니다.
+        }).exec();
+        if(!post){
+            ctx.status = 404;
+            return;
+        }
+        ctx.body = post;
+    }catch(e){
+        ctx.throw(500, e);
+    }
+};
