eaeddb92a8833a848f289c8312603dc5b42760f9
yesol-choi <ysoli3651@naver.com>
2021-11-07 17:15:39 +0900

    ObjectId

--- blog-backend/src/api/posts/index.js
+++ blog-backend/src/api/posts/index.js
@@ -5,8 +5,11 @@ const posts = new Router();
 
 posts.get('/',postsCtrl.list );
 posts.post('/',postsCtrl.write );
-posts.get('/:id',postsCtrl.read);
-posts.delete('/:id',postsCtrl.remove);
-posts.patch('/:id',postsCtrl.update);
 
+const post = new Router(); // /api/posts/:id
+posts.get('/',postsCtrl.read);
+posts.delete('/',postsCtrl.remove);
+posts.patch('/',postsCtrl.update);  //이런 리팩토링시에, 한눈에 경로가 들어오지 않는다.
+
+post.use('/:id',postsCtrl.checkObjectId, post.routes());
 export default posts;
