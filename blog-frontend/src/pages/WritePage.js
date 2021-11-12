0cdbb467307d9d153a27443266b05e5081877388
yesol-choi <ysoli3651@naver.com>
2021-11-12 16:43:43 +0900

    editor under component UI

--- blog-frontend/src/pages/WritePage.js
+++ blog-frontend/src/pages/WritePage.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import Editor from '../components/write/Editor';
 import Responsive from '../components/common/Responsive';
-
+import WriteActionButtons from '../components/write/WriteActionButtons';
+import TagBox from '../components/write/TagBox';
 const WritePage = () => {
     return (
         <Responsive>
             <Editor/>
+            <TagBox/>
+            <WriteActionButtons />
         </Responsive>
     );
 };
